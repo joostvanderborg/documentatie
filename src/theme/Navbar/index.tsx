e26cabc941099c8f6368d9d774bafce4a43327e2
Yolijn <yolijn@frameless.io>
2023-04-28 14:05:47 +0200

    wip

---
+++ src/theme/Navbar/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import type NavbarType from '@theme/Navbar';
+import type { WrapperProps } from '@docusaurus/types';
+import { PageHeader } from '@utrecht/component-library-react';
+import { NLDSLogo } from '@site/src/components/NLDSLogo';
+
+type Props = WrapperProps<typeof NavbarType>;
+
+export default function NavbarWrapper(props: Props): JSX.Element {
+  return (
+    <PageHeader style={{ zIndex: 1 }}>
+      <NLDSLogo />
+    </PageHeader>
+  );
+}
