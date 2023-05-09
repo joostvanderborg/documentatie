b05af4a899d02ba73cfe2660096478605af60815
Yolijn <yolijn@frameless.io>
2023-05-09 11:29:23 +0200

    wip: header

--- src/components/MainNav.tsx
+++ src/components/MainNav.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
 import clsx from 'clsx';
 
+import { items } from '@site/navConfig';
 import style from './MainNav.module.css';
+import Link from '@docusaurus/Link';
 
-export const MainNav = ({ children }) => {
+export const MainNav = () => {
   return (
-    <div className={clsx(style['main-nav'])}>
-      {/* For now use @theme navbar items as this helps maintain the navbar configuration while in beta*/}
-      <div className={clsx(style['main-nav__container'], 'container')}>{children}</div>
+    <div className={clsx(style['main-nav'], 'utrecht-navhtml')}>
+      <nav className="topnav">
+        <ul className="utrecht-topnav__list">
+          {items.map(({ to, href, label, current, focus, focusVisible }) => (
+            <li className="utrecht-topnav__item">
+              <Link
+                className={clsx(
+                  'utrecht-topnav__link',
+                  current && 'utrecht-topnav__link--current',
+                  focus && 'utrecht-topnav__link--focus',
+                  focusVisible && 'utrecht-topnav__link--focus-visible',
+                )}
+                href={href}
+                to={to}
+              >
+                {label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </div>
   );
 };
--- src/theme/Navbar/index.tsx
+++ src/theme/Navbar/index.tsx
@@ -3,6 +3,7 @@ import type NavbarType from '@theme/Navbar';
 import type { WrapperProps } from '@docusaurus/types';
 import { PageHeader } from '@utrecht/component-library-react';
 import { NLDSLogo } from '@site/src/components/NLDSLogo';
+import { MainNav } from '@site/src/components/MainNav';
 
 type Props = WrapperProps<typeof NavbarType>;
 
@@ -10,6 +11,7 @@ export default function NavbarWrapper(props: Props): JSX.Element {
   return (
     <PageHeader style={{ zIndex: 1 }}>
       <NLDSLogo />
+      <MainNav />
     </PageHeader>
   );
 }
